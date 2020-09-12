e85f3f07491286f25f153b1688cb258685450686
Christian Oliveros <christianol_01@hotmail.com>
2020-09-12 18:19:32 -0400

    More Tests

--- src/PingPong/Player/Stig.hs
+++ src/PingPong/Player/Stig.hs
@@ -1,6 +1,7 @@
 module PingPong.Player.Stig (stig) where
 
 import Data.Bool (bool)
+import Data.Fixed (mod')
 
 import Control.Lens
 import Data.Geometry
@@ -10,8 +11,62 @@ import PingPong.Player
 
 import Graphics.Gloss (Color, makeColor)
 
+-- Geometry Helpers
+
+-- | Threshold function
+threshold :: Float -> Float -> Float -> Float
+threshold limit target val
+    | abs diff < limit = target
+    | otherwise = val
+    where diff = target - val
+
+-- | tau = 2 * pi
+tau :: Float
+tau = 2 * pi
+
+-- | Normalize Angle in Radians between (-pi, pi]
+-- Based on https://stackoverflow.com/a/2323034
+normalizeAngle :: Float -> Float
+normalizeAngle a = 
+    let a1 = mod' a tau
+        a2 = mod' (a + tau) tau
+    in bool a2 (a2 - tau) (a2 > pi)
+
+-- | Shortest Distance Between Two Angles in Radians [-pi, pi)
+deltaAngle :: Float -> Float -> Float
+deltaAngle a1 a2 
+    | diff >= pi = diff - tau
+    | diff < -pi = tau + diff
+    | otherwise = diff
+    where 
+        a1N = normalizeAngle a1
+        a2N = normalizeAngle a2
+        diff = a1N - a2N
+
+-- End of Geometry Helpers
+
+-- Simulation Helpers
+
+-- | Get the current Joint parameters as a Motion
+getCurrentJoints :: Arm -> Motion
+getCurrentJoints [] = []
+getCurrentJoints (Joint _ rad : xs) = rad : getCurrentJoints xs
+getCurrentJoints (Link _ _ : xs) = getCurrentJoints xs
+
+-- | Motion Velocity Limit
+motionLimit :: Float 
+motionLimit = 1.0
+
+-- | Apply Motion limits to avoid simulation problems
+applyMotionLimits :: Motion -> Motion
+applyMotionLimits = map f
+    where f x = min motionLimit $ max (-motionLimit) x
+
+-- End of Simulation Helpers
+
+-- | Stig's player
 stig :: Player
-stig = Player stigArm stigFoot noAction
+stig = Player stigArm stigFoot stigAction
 
 paleBlue :: Color
 paleBlue = makeColor 0.5 0.5 0.6 1
@@ -22,6 +77,7 @@ red = makeColor 1 0 0 1
 hotPink :: Color
 hotPink = makeColor 1.0 0.2 0.7 1
 
+-- | Arm to use
 stigArm :: Arm
 stigArm = [ Joint red (-0.3) -- (0.1)
            , Link paleBlue 0.5
@@ -33,32 +89,35 @@ stigArm = [ Joint red (-0.3) -- (0.1)
            , Link hotPink 0.1 -- Bat
            ]
 
+-- | Separation from the center of the table
 stigFoot :: Float
 stigFoot = 1.3
 
--- | Get the current Joint parameters as a Motion
-getCurrentJoints :: Arm -> Motion
-getCurrentJoints [] = []
-getCurrentJoints (Joint _ rad : xs) = rad : getCurrentJoints xs
-getCurrentJoints (Link _ _ : xs) = getCurrentJoints xs
+-- | Common Threshold for stig functions
+stigThreshold :: Float -> Float -> Float
+stigThreshold = threshold 0.001
 
--- | Get the an empty Motion list from Stig's arm
+-- | Stig rest postion
+stigRest :: Motion
+stigRest = getCurrentJoints stigArm
+
+-- | Get the a zeroed Motion list for Stig's arm
 stigNoMotion :: Motion
-stigNoMotion = map f $ getCurrentJoints stigArm
+stigNoMotion = map f stigRest
     where f = const 0
 
--- | Motion Velocity Limit
-motionLimit :: Float 
-motionLimit = 1.0
+-- | Calculate Motion to Rest Position. !Warning: no limits are applied
+armToStigRestMotion :: Arm -> Motion
+armToStigRestMotion ar = zipWith f stigRest $ getCurrentJoints ar
+    where 
+        g = stigThreshold 0.0
+        f = deltaAngle . g
 
--- | Apply Motion limits to avoid simulation problems
-applyMotionLimits :: Motion -> Motion
-applyMotionLimits = map f
-    where f x = min motionLimit $ max (-motionLimit) x
 
-noAction :: BallState -> Arm -> IO Motion
-noAction bs arm = 
+stigAction :: BallState -> Arm -> IO Motion
+stigAction bs arm = 
     return $ 
     let xdir = view xComponent $ dir bs
-        curJoints = getCurrentJoints arm
-    in bool stigNoMotion [1, 1, 1, -1] (xdir > 0)
+        toRest = armToStigRestMotion arm
+        motion = bool [1, -1, 1, -1] toRest (xdir > 0)
+    in applyMotionLimits motion -- Velocity limits
